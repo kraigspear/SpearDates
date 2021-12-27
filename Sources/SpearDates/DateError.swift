95cc72e8a47c26fb28077699167ae4d230f5f9b9
Kraig Spear <kraigspear@gmail.com>
2021-12-27 05:22:36 -0500

    Adjust the time on a given day

---
+++ Sources/SpearDates/DateError.swift
@@ -0,0 +1,13 @@
+//
+//  DateError.swift
+//  
+//
+//  Created by Kraig Spear on 12/26/21.
+//
+
+import Foundation
+
+public enum DateError: Error {
+    /// Attempt to create a date failed
+    case dateNotCreated
+}
