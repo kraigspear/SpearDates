c2e5b8abe71805f1ff53d58d9224a2952b9261d5
Kraig Spear <kraigspear@gmail.com>
2025-04-22 06:03:03 -0400

    Swift Format - Function for format time (#11)
    * Swift Format - Function for format time
    
    * Test subtraction operator

--- Tests/SpearDatesTests/DateFormatTest.swift
+++ Tests/SpearDatesTests/DateFormatTest.swift
@@ -6,14 +6,12 @@
 //
 
 import Foundation
-import Testing
 @testable import SpearDates
+import Testing
 
 struct FormatTest {
-    
     @Test("Zulu formatting")
     func zulu() {
-        
         let dateComponents = DateComponents(
             timeZone: TimeZone(secondsFromGMT: 0),
             year: 2024,
@@ -23,13 +21,40 @@ struct FormatTest {
             minute: 30,
             second: 0
         )
-        
+
         let calendar = Calendar(identifier: .gregorian)
         let date = calendar.date(from: dateComponents)!
-        
+
         let formatted = DateFormatters.formatZulu(date)
-        #expect("2024-08-06T14:30:00Z" == formatted)
-        
+        #expect(formatted == "2024-08-06T14:30:00Z")
     }
-    
+
+    struct HourString {
+        @Test("Short Hour")
+        func shortHour() {
+            #expect(DateFormatters.formatHourAmPm(2) == "2 AM")
+            #expect(DateFormatters.formatHourAmPm(12) == "12 PM")
+            #expect(DateFormatters.formatHourAmPm(17) == "5 PM")
+        }
+
+        @Test("No Spaces")
+        func noSpaces() {
+            #expect(DateFormatters.formatHourAmPm(2, spaceBetweenHourAndAmPm: false) == "2AM")
+        }
+
+        @Test("Lower cased")
+        func lowerCased() {
+            #expect(DateFormatters.formatHourAmPm(2, lowerCased: true) == "2 am")
+        }
+
+        @Test("Lower cased and no spaces")
+        func lowerCasedNoSpaces() {
+            #expect(DateFormatters.formatHourAmPm(
+                2,
+                lowerCased: true,
+                spaceBetweenHourAndAmPm: false
+            ) == "2am"
+            )
+        }
+    }
 }
